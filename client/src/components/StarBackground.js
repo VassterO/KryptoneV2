2076ad6a8e4db5cbaf4d7ea93e0115e9d4b7f185
VassterO <161985347+VassterO@users.noreply.github.com>
2024-05-29 23:19:05 +0200

    soy inutil

--- client/src/components/StarBackground.js
+++ client/src/components/StarBackground.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 // Función para generar una estrella con animación
-const generateStar = (index) => {
+const generateStar = (index, sizeFactor) => {
     const speed = 4 + Math.random() * 2; // Velocidad entre 4s y 6s
-    const sizeFactor = window.innerWidth >= 2560 ? 1 : window.innerWidth / 2560;
     const size = (Math.random() * 2 + 3) * sizeFactor; // Tamaño ajustado proporcionalmente
     const delay = Math.random() * 2; // Retardo inicial hasta 2s
 
@@ -20,7 +19,6 @@ const generateStar = (index) => {
                 animationDelay: `${delay}s`,
                 opacity: 0,
                 animationFillMode: 'forwards',
-                animationPlayState: window.backgroundAnim ? 'running' : 'paused'
             }}
         ></div>
     );
@@ -31,16 +29,18 @@ const StarBackground = () => {
     const [stars, setStars] = useState([]);
 
     useEffect(() => {
+        const sizeFactor = window.innerWidth >= 2560 ? 1 : window.innerWidth / 2560;
         const numStars = Math.floor((window.innerWidth * window.innerHeight) / (2560 * 1440) * 50); // Ajustar cantidad basado en la resolución
+
         // Genera las estrellas iniciales
-        const initialStars = Array.from({ length: numStars }, (_, index) => generateStar(index));
+        const initialStars = Array.from({ length: numStars }, (_, index) => generateStar(index, sizeFactor));
         setStars(initialStars);
 
         // Añade nuevas estrellas periódicamente hasta un máximo ajustado
         const interval = setInterval(() => {
             setStars((prevStars) => {
                 if (prevStars.length < numStars * 2) {
-                    return [...prevStars, generateStar(prevStars.length)];
+                    return [...prevStars, generateStar(prevStars.length, sizeFactor)];
                 }
                 clearInterval(interval);
                 return prevStars;
@@ -48,25 +48,6 @@ const StarBackground = () => {
         }, 500);
 
         return () => clearInterval(interval);
-    }, []);
-
-    // Observa los cambios en la variable global
-    useEffect(() => {
-        const observer = new MutationObserver(() => {
-            const stars = document.querySelectorAll('.star');
-            stars.forEach((star) => {
-                star.style.animationPlayState = window.backgroundAnim ? 'running' : 'paused';
-            });
-        });
-
-        observer.observe(document.documentElement, {
-            attributes: true,
-            attributeFilter: ['data-background-anim']
-        });
-
-        return () => {
-            observer.disconnect();
-        };
     }, []);
 
     return (
