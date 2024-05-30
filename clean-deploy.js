06871b5cd9c30ac6216ec2afb98feb7d46b239f1
VassterO <161985347+VassterO@users.noreply.github.com>
2024-05-30 02:40:05 +0200

    push

---
+++ clean-deploy.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+// Define the paths to clean up
+const pathsToDelete = [
+    path.join(__dirname, 'client', 'src'),
+    path.join(__dirname, 'client', 'node_modules'),
+    path.join(__dirname, 'client', '.env'),
+    path.join(__dirname, 'client', 'public', 'static', 'js', 'components'),
+    path.join(__dirname, 'client', 'public', 'static', 'js', 'context'),
+];
+
+// Function to delete files and directories recursively
+const deleteRecursively = (targetPath) => {
+    if (fs.existsSync(targetPath)) {
+        if (fs.lstatSync(targetPath).isDirectory()) {
+            fs.readdirSync(targetPath).forEach((file) => {
+                const curPath = path.join(targetPath, file);
+                deleteRecursively(curPath);
+            });
+            fs.rmdirSync(targetPath);
+        } else {
+            fs.unlinkSync(targetPath);
+        }
+    }
+};
+
+// Clean up the specified paths
+pathsToDelete.forEach(deleteRecursively);
