cbaa7dbcf9f1f18f3c74257374fc5a5588aead40
VassterO <161985347+VassterO@users.noreply.github.com>
2024-05-30 03:15:31 +0200

    a

--- clean-up.js
+++ clean-up.js
@@ -1,29 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 
-// Paths to be deleted
-const pathsToDelete = [
-    path.join(__dirname, 'client', 'src'),
-    path.join(__dirname, 'client', 'node_modules'),
-    path.join(__dirname, 'client', 'public'),
-];
-
-// Function to delete files and directories recursively
-const deleteRecursively = (targetPath) => {
-    if (fs.existsSync(targetPath)) {
-        if (fs.lstatSync(targetPath).isDirectory()) {
-            fs.readdirSync(targetPath).forEach((file) => {
-                const curPath = path.join(targetPath, file);
-                deleteRecursively(curPath);
-            });
-            fs.rmdirSync(targetPath);
-        } else {
-            fs.unlinkSync(targetPath);
-        }
+const deleteFolderRecursive = (folderPath) => {
+    if (fs.existsSync(folderPath)) {
+        fs.readdirSync(folderPath).forEach((file, index) => {
+            const curPath = path.join(folderPath, file);
+            if (fs.lstatSync(curPath).isDirectory()) {
+                deleteFolderRecursive(curPath);
+            } else {
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(folderPath);
     }
 };
 
-// Clean up specified paths
-pathsToDelete.forEach(deleteRecursively);
+const foldersToDelete = [
+    'client/src',
+    'client/.idea',
+    'client/node_modules',
+    // add other folders if necessary
+];
 
-console.log('Clean-up completed after build');
+foldersToDelete.forEach(folder => {
+    const folderPath = path.join(__dirname, folder);
+    deleteFolderRecursive(folderPath);
+});
