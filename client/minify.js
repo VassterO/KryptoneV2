4bba9dd650ddcef377265804c33d8be10c5723bd
VassterO <161985347+VassterO@users.noreply.github.com>
2024-05-31 02:45:54 +0200

    zzzz

--- client/minify.js
+++ client/minify.js
@@ -4,19 +4,36 @@ const terser = require('terser');
 
 const directory = join(__dirname, 'build', 'static', 'js');
 
-readdirSync(directory).forEach(file => {
-    if (file.endsWith('.js')) {
-        const filePath = join(directory, file);
-        const result = terser.minify(readFileSync(filePath, 'utf8'), {
-            compress: true,
-            mangle: true
-        });
+async function minifyFiles() {
+    const files = readdirSync(directory);
 
-        if (result.error) {
-            console.error('Error minifying', filePath, result.error);
-        } else {
-            writeFileSync(filePath, result.code, 'utf8');
-            console.log('Minified', filePath);
+    for (const file of files) {
+        if (file.endsWith('.js')) {
+            const filePath = join(directory, file);
+            const fileContents = readFileSync(filePath, 'utf8');
+            try {
+                const result = await terser.minify(fileContents, {
+                    compress: true,
+                    mangle: true
+                });
+
+                if (result.error) {
+                    console.error('Error minifying', filePath, result.error);
+                } else if (result.code === undefined) {
+                    console.error('Error: Minification result is undefined for', filePath, result);
+                } else {
+                    writeFileSync(filePath, result.code, 'utf8');
+                    console.log('Minified', filePath);
+                }
+            } catch (err) {
+                console.error('Error minifying', filePath, err);
+            }
         }
     }
+}
+
+minifyFiles().then(() => {
+    console.log('Minification complete.');
+}).catch(err => {
+    console.error('Error during minification process:', err);
 });
