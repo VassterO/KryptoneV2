808bb515750299468d13529fd13837e5a97c3fdb
VassterO <161985347+VassterO@users.noreply.github.com>
2024-06-08 23:33:10 +0200

    a

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
 import { Helmet } from 'react-helmet';
 import Navbar from './components/Navbar';
